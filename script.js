395e2b65d1ae55b5e8fdc1792eb831ab6af1b98c
Arnold Sanchez <113864940+Arniezzy@users.noreply.github.com>
2022-10-12 14:42:02 -0700

    Added prompt to confirm password length

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// 
-var lowerList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var charList = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
-var numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Lowercase, Uppercase, Numeric numbers and Special Characters
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var charList = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Prompt to confirm password length
+function generatePassword() {
+  var confirmLength = (prompt("Password Length (8 -128) characters"));
+  while(confirmLength <=7 || confirmLength >=128) {
+    alert("Password length must be between (8-128)characters");
+    var confirmLength = (prompt("How many characters would you like your password to contain?"));
+  }
+  alert(`Your password will have ${confirmLength} characters`);
+}
 
 // Write password to the #password input
 function writePassword() {
